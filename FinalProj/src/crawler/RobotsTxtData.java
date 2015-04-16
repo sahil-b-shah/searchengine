ae788a1a8582686848d611a38bc2b9cedabf31e6
salowed <salowed@gmail.com>
2015-04-16 11:07:50 -0400

    Added entity class for robots.txt

---
+++ FinalProj/src/crawler/RobotsTxtData.java
@@ -0,0 +1,40 @@
+package crawler;
+
+import java.util.ArrayList;
+
+import com.sleepycat.persist.model.Entity;
+import com.sleepycat.persist.model.PrimaryKey;
+
+@Entity
+public class RobotsTxtData{
+
+	@PrimaryKey
+	private String hostName;
+	private ArrayList<String> allowedLinks;
+	private ArrayList<String> disallowedLinks;
+	private long crawlDelay;
+	
+	public RobotsTxtData(String hostName, ArrayList<String> allowedLinks,  ArrayList<String> disallowedLinks, long crawlDelay)
+	{
+		this.hostName = hostName;
+		this.allowedLinks = allowedLinks;
+		this.disallowedLinks = disallowedLinks;
+		this.crawlDelay = crawlDelay;
+	}
+	
+	public final String getHostName(){
+		return hostName;
+	}
+	
+	public final ArrayList<String> getAllowedLinks(){
+		return allowedLinks;	
+	}
+	
+	public final ArrayList<String> getDisallowedLinks(){
+		return allowedLinks;	
+	}
+	
+	public final long getCrawlDelay(){
+		return crawlDelay;
+	}
+}
--- FinalProj/src/crawler/RobotsTxtValue.java
+++
@@ -1,9 +0,0 @@
-package crawler;
-
-import com.sleepycat.persist.model.Entity;
-import com.sleepycat.persist.model.PrimaryKey;
-
-@Entity
-public class RobotsTxtValue {
-
-}
