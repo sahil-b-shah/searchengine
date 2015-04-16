db04be7f489629968e851629b355f097d06bb8c3
salowed <salowed@gmail.com>
2015-04-16 11:45:42 -0400

    Added package crawler.storage and moved files

--- FinalProj/src/crawler/storage/RobotsDBWrapper.java
+++ FinalProj/src/crawler/storage/RobotsDBWrapper.java
@@ -1,4 +1,4 @@
-package crawler;
+package crawler.storage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
--- FinalProj/src/crawler/storage/RobotsTxtData.java
+++ FinalProj/src/crawler/storage/RobotsTxtData.java
@@ -1,4 +1,4 @@
-package crawler;
+package crawler.storage;
 
 import java.util.ArrayList;
 
