ccf42643d8d2674b8d5ce90e606e97ee9c70be16
salowed <salowed@gmail.com>
2015-04-16 12:59:14 -0400

    safsaf

---
+++ FinalProj/src/crawler/storage/URLFrontierDBWrapper.java
@@ -0,0 +1,147 @@
+package crawler.storage;
+
+import java.io.File;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import com.sleepycat.je.DatabaseException;
+import com.sleepycat.je.Environment;
+import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.persist.EntityStore;
+import com.sleepycat.persist.PrimaryIndex;
+import com.sleepycat.persist.StoreConfig;
+
+public class URLFrontierDBWrapper {
+	private String envDirectory = null;
+	private File envFile;
+	
+	private Environment myEnv;
+	private EntityStore store;
+	
+	private PrimaryIndex<Integer, URLFrontierData> urlFrontier;
+	//private int channelId = 0;
+	
+	private static URLFrontierDBWrapper db;
+	private static boolean isFirstThread = true;
+	
+	public synchronized static URLFrontierDBWrapper getInstance(String homeDirectory) {
+		if (db == null) {
+			System.out.println("Making new db wrapper");
+			db = new URLFrontierDBWrapper(homeDirectory);
+		}
+		return db;
+	}
+	
+	private URLFrontierDBWrapper(String homeDirectory) {
+		envDirectory = homeDirectory;
+		System.out.println("Opening environment in: " + envDirectory);
+		envFile = new File(envDirectory);
+		envFile.mkdirs();
+		setup();
+	}
+	
+	private void setup() {
+
+		try {
+	        EnvironmentConfig envConfig = new EnvironmentConfig();
+	        StoreConfig storeConfig = new StoreConfig();
+	        //envConfig.setTransactional(true);
+	        envConfig.setAllowCreate(true);
+	        storeConfig.setAllowCreate(true);
+	        //storeConfig.setTransactional(true);
+	
+	        myEnv = new Environment(envFile, envConfig);
+	        store = new EntityStore(myEnv, "entityStore", storeConfig);
+		} catch (DatabaseException dbe) {
+			System.err.println("Error opening environment and store: " + dbe.toString());
+			System.exit(-1);
+		}
+		
+		try {
+			urlFrontier = store.getPrimaryIndex(Integer.class, URLFrontierData.class);
+			
+		} catch (DatabaseException dbe) {
+			System.err.println("Error making indexes");
+			System.exit(-1);
+		}
+	}
+	
+	public synchronized void close() {
+		if (store != null) {
+			try {
+				store.close();
+			} catch (DatabaseException dbe) {
+				System.err.println("Error closing store: " + dbe.toString());
+				System.exit(-1);
+			}
+		}
+		
+		if (myEnv != null) {
+			try {
+				myEnv.close();
+			} catch (DatabaseException dbe) {
+				System.err.println("Error closing environment: " + dbe.toString());
+				System.exit(-1);
+			}
+		}
+		db = null;
+	}
+	
+	public synchronized final Environment getEnvironment() {
+		return myEnv;
+	}
+	
+	public synchronized final EntityStore getStore() {
+		return store;
+	}
+	
+	
+	public synchronized Entry<Integer, URLFrontierData> getLastUrl() {
+		TreeMap<Integer, URLFrontierData> orderedFrontier = new TreeMap<Integer, URLFrontierData>(urlFrontier.map());
+		Entry<Integer, URLFrontierData> e = orderedFrontier.lastEntry();
+		
+		return e;
+	}
+	
+	public synchronized Entry<Integer, URLFrontierData> getNextUrl() {
+		TreeMap<Integer, URLFrontierData> orderedFrontier = new TreeMap<Integer, URLFrontierData>(urlFrontier.map());
+		Entry<Integer, URLFrontierData> e = orderedFrontier.firstEntry();
+		if (e!=null) {
+			boolean success = urlFrontier.delete(e.getKey());
+			//System.out.println(e.getValue().getUrl()+": "+success);
+		}
+		return e;
+	}
+	
+	public synchronized boolean isEmpty() {
+		return (urlFrontier.count() == 0);
+	}
+	
+	public synchronized void addUrl(String docURL) {
+		/*if(docURL == null || docURL.equals(""))
+			return true;
+		docURL = docURL.trim();
+		if(docURL.startsWith("http://")) {
+			docURL = docURL.substring(7);
+		} else if (docURL.startsWith("https://")) {
+			docURL = docURL.substring(8);
+		}
+		if (docURL.isEmpty()) {
+			return;
+		}*/
+		
+		System.out.println("Adding " + docURL+ " to db");
+		int id;
+		if(getLastUrl() == null) {
+			id = 0;
+		} else {
+			id = getLastUrl().getKey();
+		}
+		System.out.println(id+1);
+		URLFrontierData q_url = new URLFrontierData();
+		q_url.setId(id+1);
+		q_url.setUrl(docURL);
+		urlFrontier.put(q_url);
+	}
+		
+}
